2a25766e4cec136e386eff8337120971f5dcd6f9
Luís Sousa <luiscnsousa@outlook.com>
2023-04-15 23:28:43 +0100

    add super-duper custom javascript action that uploads code to AWS S3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,19 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello from my custom JavaScript action');
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // Octokit can be used to communicate with GitHub API
+    // github.getOctokit(...)
+
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice('Files successfully uploaded to S3');
 }
 
 run();
